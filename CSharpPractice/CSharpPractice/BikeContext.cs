02ae04b6ac9f69d7aa8c70e72ae097019887262f
MX\jyoo <jyoo@themxgroup.com>
2016-11-16 16:30:44 -0600

    Adding Entity Framework and initial BikeContext

---
+++ CSharpPractice/CSharpPractice/BikeContext.cs
@@ -0,0 +1,31 @@
+namespace CSharpPractice
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class BikeContext : DbContext
+    {
+        // Your context has been configured to use a 'BikeContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'CSharpPractice.BikeContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'BikeContext' 
+        // connection string in the application configuration file.
+        public BikeContext()
+            : base("name=BikeContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}

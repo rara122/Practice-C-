bd5e8064933685333a42b44f29c59cfee7b99421
MX\jyoo <jyoo@themxgroup.com>
2016-11-16 16:40:00 -0600

    Adding Bike

---
+++ CSharpPractice/CSharpPractice/Bike.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    public class Bike
+    {
+        public int BikeId { get; set; }
+        public string BikeName { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
--- CSharpPractice/CSharpPractice/BikeContext.cs
+++ CSharpPractice/CSharpPractice/BikeContext.cs
@@ -20,7 +20,7 @@ namespace CSharpPractice
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<Bike> Bikes { get; set; }
     }
 
     //public class MyEntity
